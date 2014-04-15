6462c554bc0d53d6d7fe2a8fb8f1e6e3d55ee4ee
Volodymyr Cherepanyak <chervol@quintagroup.com>
2014-04-15 14:31:01 +0300

    added dexterity compatibility into diff view

--- plone/app/iterate/browser/diff.py
+++ plone/app/iterate/browser/diff.py
@@ -8,19 +8,42 @@ from Products.Five.browser import BrowserView
 from plone.app.iterate.interfaces import IWorkingCopy, IBaseline
 from plone.app.iterate.relation import WorkingCopyRelation
 
+import pkg_resources
+from plone.app.iterate.interfaces import IIterateAware
+
+try:
+    pkg_resources.get_distribution('plone.app.stagingbehavior')
+    from plone.app.stagingbehavior.utils import get_working_copy
+    from plone.app.stagingbehavior.utils import get_baseline
+except pkg_resources.DistributionNotFound:
+    HAS_STAGINGBEHAVIOR = False
+else:
+    HAS_STAGINGBEHAVIOR = True
+
+
 class DiffView( BrowserView ):
 
     def __init__( self, context, request ):
         self.context = context
         self.request = request
-        if IBaseline.providedBy( self.context ):
-            self.baseline = context
-            self.working_copy = context.getBackReferences( WorkingCopyRelation.relationship )[0]
-        elif IWorkingCopy.providedBy( self.context ):
-            self.working_copy = context
-            self.baseline = context.getReferences( WorkingCopyRelation.relationship )[0]
+        if HAS_STAGINGBEHAVIOR and IIterateAware.providedBy(context):
+            if IBaseline.providedBy( self.context ):
+                self.baseline = get_baseline(context)
+                self.working_copy = get_working_copy(context)
+            elif IWorkingCopy.providedBy( self.context ):
+                self.working_copy = get_working_copy(context)
+                self.baseline = get_baseline(context)
+            else:
+                raise AttributeError("Invalid Context")
         else:
-            raise AttributeError("Invalid Context")
+            if IBaseline.providedBy( self.context ):
+                self.baseline = context
+                self.working_copy = context.getBackReferences( WorkingCopyRelation.relationship )[0]
+            elif IWorkingCopy.providedBy( self.context ):
+                self.working_copy = context
+                self.baseline = context.getReferences( WorkingCopyRelation.relationship )[0]
+            else:
+                raise AttributeError("Invalid Context")
 
     def diffs( self ):
         diff = getToolByName(self.context, 'portal_diff')
